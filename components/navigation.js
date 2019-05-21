9c46d82912631663dee84e615e69f71c8f563286
Cesar Aponte <cesar.aponte.1996.vazquez@gmail.com>
2019-05-21 16:45:54 -0400

    Have been working on tablet and mobile views for navigation bar. Working on the javascript portions so that a traditional naviagation bar is present in desktop view and a hamburger menu is present in tablet/phone view. Currently media queries not working to hide the hamburger from the desktop view. Min-width and max-width are both none responsive with the hamburger library.

--- components/navigation.js
+++ components/navigation.js
@@ -1,6 +1,15 @@
 
-const navigationMenuBtn = document.querySelector(".hamburger")
+const navigationMenuBtn = document.querySelector(".hamburger");
+const navigationBar = document.querySelector(".navigationBar");
+
 
 navigationMenuBtn.addEventListener("click", () =>{
     navigationMenuBtn.classList.toggle("is-active")
+    navigationBar.classList.toggle("showflex")
 })
+
+window.addEventListener("scroll", () =>{
+    navigationMenuBtn.classList.remove("is-active")
+    navigationBar.classList.remove("showflex")
+})
+
